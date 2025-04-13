ec4963d4bd84d14e97bd762a735af9f61e589b57
Startoto1007 <gouessestar@gmail.com>
2025-04-13 11:20:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} est connecté au serveur Discord!')
-    
+
     # Chargement des cogs (modules)
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
             await bot.load_extension(f'cogs.{filename[:-3]}')
             print(f'Module {filename} chargé avec succès.')
-    
+
     # Définition du statut du bot
     await bot.change_presence(activity=discord.Game(name="!help pour voir les commandes"))
 
