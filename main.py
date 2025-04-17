efc3874c74f32fbd12a4d5fd1df455372d3e7d19
Startoto1007 <gouessestar@gmail.com>
2025-04-17 09:33:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,13 +15,16 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f"Bot connecté en tant que {bot.user}")
-    # Charger les cogs
     try:
-        await bot.load_extension("cogs.moderation")  # Pour la modération
-        await bot.load_extension("cogs.ticket")      # Pour les tickets
+        # Charger les extensions
+        await bot.load_extension("cogs.moderation")
+        await bot.load_extension("cogs.ticket")
     except Exception as e:
         print(f"Erreur lors du chargement des cogs : {e}")
-    await bot.tree.sync()  # Synchroniser les commandes slash
+
+    # Synchroniser les commandes slash
+    await bot.tree.sync()
+    print("Commandes synchronisées.")
     
 # Démarrage du bot avec le token
 bot.run(DISCORD_TOKEN)
