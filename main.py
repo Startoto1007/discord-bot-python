4e0458d151e6c5dd4d0c2017d70b4d0a27ae8e23
Startoto1007 <gouessestar@gmail.com>
2025-04-17 09:37:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 import discord
 from discord.ext import commands
 import os
+
+# Charger les variables d'environnement
 from dotenv import load_dotenv
-
-# Chargement des variables d'environnement
 load_dotenv()
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
+# Initialisation du bot
 intents = discord.Intents.default()
-intents.message_content = True
+intents.members = True  # Nécessaire si tu veux interagir avec les membres du serveur
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+# Charger les cogs (commandes) au démarrage du bot
 @bot.event
 async def on_ready():
     print(f"Bot connecté en tant que {bot.user}")
+    
     try:
-        # Charger les extensions
-        await bot.load_extension("cogs.moderation")
-        await bot.load_extension("cogs.ticket")
+        # Charger l'extension des commandes depuis le dossier /cogs/commands.py
+        await bot.load_extension("cogs.commands")
     except Exception as e:
         print(f"Erreur lors du chargement des cogs : {e}")
-
-    # Synchroniser les commandes slash
+    
+    # Synchroniser les commandes slash avec Discord
     await bot.tree.sync()
     print("Commandes synchronisées.")
-    
-# Démarrage du bot avec le token
-bot.run(DISCORD_TOKEN)
+
+# Token de connexion
+token = os.getenv("DISCORD_TOKEN")
+
+# Démarrer le bot avec le token
+bot.run(token)
